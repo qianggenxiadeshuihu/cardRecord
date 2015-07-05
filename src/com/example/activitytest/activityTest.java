53e496409b18f245f38175a3c6d02fddc985f729
qianggenxiadeshuihu <hujiayi.he29@163.com>
2015-07-05 21:57:20 +0800

    first release

--- src/com/example/activitytest/activityTest.java
+++ src/com/example/activitytest/activityTest.java
@@ -2,28 +2,60 @@ package com.example.activitytest;
 
 
 import java.security.PublicKey;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import android.R.string;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.StaticLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewDebug.FlagToString;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.net.Uri;
 
 
 public class activityTest extends Activity {
 
-	public static String phoneNumber;
-	public static String displayInfo;
+	public static Timer global_time;
+	public static int flag = 0;
+	public static int count_A = 0;
+	public static int count_B = 0;
+	public static int count_C = 0;
+	public static int count_D = 0;
 	
+	public static TextView name_a;
+	public static TextView name_b;
+	public static TextView name_c;
+	public static TextView name_d;
+	
+
+	public static TextView name_A;
+	public static TextView name_B;
+	public static TextView name_C;
+	public static TextView name_D;
+	
+
+	public static TextView count_a;
+	public static TextView count_b;
+	public static TextView count_c;
+	public static TextView count_d;
+
+	public static TimerTask t_task;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -32,7 +64,59 @@ public class activityTest extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.layout_1);
 		
+
+		name_a = (TextView)findViewById(R.id.name_a);
+		name_b = (TextView)findViewById(R.id.name_b);
+		name_c = (TextView)findViewById(R.id.name_c);
+		name_d = (TextView)findViewById(R.id.name_d);
 		
+		name_A = (EditText)findViewById(R.id.name_A);
+		name_B = (EditText)findViewById(R.id.name_B);
+		name_C = (EditText)findViewById(R.id.name_C);
+		name_D = (EditText)findViewById(R.id.name_D);
+		
+		count_a = (TextView)findViewById(R.id.count_a);
+		count_b = (TextView)findViewById(R.id.count_b);
+		count_c = (TextView)findViewById(R.id.count_c);
+		count_d = (TextView)findViewById(R.id.count_d);
+
+		
+		global_time = new Timer();
+		
+		final Handler my_handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg)
+			{
+				super.handleMessage(msg);
+				if (msg.what == 100)
+				{
+					activityTest.count_a.setText(String.valueOf(count_A));
+					activityTest.count_b.setText(String.valueOf(count_B));
+					activityTest.count_c.setText(String.valueOf(count_C));
+					activityTest.count_d.setText(String.valueOf(count_D));
+					return;
+				}
+				switch (flag)
+				{
+				case 0:
+					activityTest.name_a.setText(activityTest.name_A.getText().toString());
+					activityTest.count_a.setText(String.valueOf(count_A));
+					break;
+				case 1:
+					activityTest.name_b.setText(activityTest.name_B.getText().toString());
+					activityTest.count_b.setText(String.valueOf(count_B));
+					break;
+				case 2:
+					activityTest.name_c.setText(activityTest.name_C.getText().toString());
+					activityTest.count_c.setText(String.valueOf(count_C));
+					break;
+				case 3:
+					activityTest.name_d.setText(activityTest.name_D.getText().toString());
+					activityTest.count_d.setText(String.valueOf(activityTest.count_D));
+					break;
+				}
+			}
+		};
 		
 		
 		Button button1 = (Button)findViewById(R.id.start_timer);
@@ -41,49 +125,77 @@ public class activityTest extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(activityTest.this, "you seems want to input phone number", Toast.LENGTH_SHORT).show();
-				Intent intent = new Intent(getPackageName()+".SECOND");
-				startActivity(intent);
+				t_task = new TimerTask() {	
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						Message msg = new Message();
+						
+						switch (flag)
+						{
+						case 0:
+							count_A += 1;
+							msg.what = count_A;
+							break;
+						case 1:
+							count_B += 1;
+							msg.what = count_B;
+							break;
+						case 2:
+							count_C += 1;
+							msg.what = count_C;
+							break;
+						case 3:
+							count_D += 1;
+							msg.what = count_D;
+							break;
+						default:
+							Log.e("hi", "overflow for flag");
+						}
+						my_handler.sendMessage(msg);
+					}
+				};
+				global_time.scheduleAtFixedRate(t_task, 0, 1000);		
 			}
 		});
 		
-		Button button2 = (Button)findViewById(R.id.button_info_display);
+		Button button2 = (Button)findViewById(R.id.stop_timer);
 		button2.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(activityTest.this, "you seems want to input display", Toast.LENGTH_SHORT).show();
-				Intent intent = new Intent(getPackageName()+".THIRD");
-				startActivity(intent);
+				t_task.cancel();
+				
 			}
 		});
 		
-		Button button3 = (Button)findViewById(R.id.button_dial_out);
+		Button button3 = (Button)findViewById(R.id.reset_timer);
 		button3.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if (phoneNumber.length() == 0) {					
-					Toast.makeText(activityTest.this, "You have not input phone number", Toast.LENGTH_SHORT).show();
-				}
-				if (displayInfo.length() == 0) {					
-					Toast.makeText(activityTest.this, "You have not input display info", Toast.LENGTH_SHORT).show();
-				} else {
-					NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-					Notification notification = new Notification(R.drawable.ic_launcher, "display info", System.currentTimeMillis());
-					notification.setLatestEventInfo(activityTest.this, "display info", activityTest.displayInfo, null);
-					manager.notify(1, notification);
-				}
-				Intent intent = new Intent(Intent.ACTION_DIAL);
-				intent.setData(Uri.parse("tel:"+activityTest.phoneNumber));
-				startActivity(intent);
+				count_A = 0;
+				count_B = 0;
+				count_C = 0;
+				count_D = 0;
+				flag = 0;
+				Message msg = new Message();
+				msg.what = 100;
+				my_handler.sendMessage(msg);
 			}
 		});
 		
-		
+		Button button4 = (Button)findViewById(R.id.switch_beginner);
+		button4.setOnClickListener(new View.OnClickListener() {
 			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				flag = (flag+1) % 4;  
+			}
+		});			
 	}
 
 	@Override
@@ -97,12 +209,6 @@ public class activityTest extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
 		switch (item.getItemId()) {
-		case R.id.add_item:
-			Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.remove_item:
-			Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
-			break;
 		case R.id.exit_item:
 			finish();
 			break;
@@ -112,7 +218,6 @@ public class activityTest extends Activity {
 		}
 		return true;
 	}
-
 
 }
 
